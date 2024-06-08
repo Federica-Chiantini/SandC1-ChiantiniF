ba011266bdea535adc12abdaff62594921a9f245
Federica Chiantini <fede.chiantini@gmail.com>
2024-06-08 12:41:19 +0200

    aggiunto ngclass per colorare card

--- src/app/components/transaction-item/transaction-item.component.ts
+++ src/app/components/transaction-item/transaction-item.component.ts
@@ -8,5 +8,16 @@ import { cardMenu } from '../../models/cardTransaction';
 })
 export class TransactionItemComponent {
     @Input()
-    VociM?: cardMenu;
+    VociM ?: cardMenu;
+
+getSfondo() { //creato metodo per controllare se quando viene generato il VociM?.type corrisponde a un valore a cui assegno poi la classe corrispondente
+        if(this.VociM?.type === 'invoice'){
+          return 'inv' //classe .inv dentro style.css
+        }
+        if(this.VociM?.type === 'withdrawal'){
+          return 'with' //classe .with dentro style.css
+        }else{
+          return //terzo caso in cui valore non e' uguali a quelli sovrastanti
+        }
+    }
 }
