aec7c3e0b993cd53debd9248a5c07adc358f9c16
Federica Chiantini <fede.chiantini@gmail.com>
2024-06-17 15:00:30 +0200

    aggiunta ngstyle

--- src/app/components/transaction-item/transaction-item.component.ts
+++ src/app/components/transaction-item/transaction-item.component.ts
@@ -10,7 +10,6 @@ export class TransactionItemComponent {
     @Input()
     VociM ?: cardMenu;
 
-//PER MATTEO: ho partecipato al tutoraggio di mercoledi' 11/06 e, visto che avevo trovato online come soluzione ngClass, ho chiesto ad Antonio di mostrarmi un esempio su come usarla. Qui l'ho implementata in una maniera simile di come l'ho vista fare a lui. Nel codice di transaction-item.component.html ho inserito ngClass con operatore ternario, credo vada bene comunque. Aspetto a lezione per vedere come lo avresti fatto tu, grazie mille e a lunedi'.
 getSfondo() { 
   return{
     'inv' : this.VociM?.type == 'invoice', //classe 'inv' assegnata alla condizione "this.VociM?.type == 'invoice'"
